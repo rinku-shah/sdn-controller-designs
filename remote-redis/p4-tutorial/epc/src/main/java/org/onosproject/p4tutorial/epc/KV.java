f80e9ea9b12feb50d8f51b24dd0b6bbf038c26c8
rinku-shah <rinku@cse.iitb.ac.in>
2020-03-21 13:27:31 +0530

    updated kv.java with get/set func

--- remote-redis/p4-tutorial/epc/src/main/java/org/onosproject/p4tutorial/epc/KV.java
+++ remote-redis/p4-tutorial/epc/src/main/java/org/onosproject/p4tutorial/epc/KV.java
@@ -20,25 +20,44 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class KV {
     private static final Logger log = getLogger(EpcApp.class);
 
+    public static void set(String key, String value){
+		try{
+			r.call("SET", key, value);
+		} catch (IOException e) {
+            e.printStackTrace();
+			log.warn("SET: Redis exception occured");
+        }
+	}
+	public static String get(String key){
+		try{
+			String out = new String((byte[])(r.call("GET", key));
+			return out;
+		} catch (IOException e) {
+            e.printStackTrace();
+			log.warn("GET: Redis exception occured");
+        }
+    }
+
 	public KV() {
 //		Jedis jedis = new Jedis("192.168.100.1");		
 		//log.warn("Jedis connected");
 		try{
 			nl.melp.redis.Redis r = new nl.melp.redis.Redis(new Socket("127.0.0.1", 6379));
-		for(int i=1; i<=100000; i++){
-			r.call("SET", "foo"+i, Integer.toString(i));
+			r.call("FLUSHALL");
+
+		for(int i=1; i<=10; i++){
+			//r.call("SET", "foo"+i, Integer.toString(i));
+			set("foo"+i, Integer.toString(i));
 		}
 		//r.call("INCRBY", "foo", "456");
-		String out = new String((byte[])(r.call("GET", "foo25")));
+		//String out = new String((byte[])(r.call("GET", "foo25")));
 		//String out = Base64.getEncoder().encodeToString(r.call("GET", "foo"));
-		log.warn("GET value = {}", out); // will print '579'
+		log.warn("GET value = {}", get("foo10")); // will print '579'
 
                 } catch (IOException e) {
                         e.printStackTrace();
 			log.warn("Redis exception occured");
                 }
-
-
 		//RedisURI redisUri = RedisURI.Builder.redis("localhost")
                                 //.withPassword("authentication")
                                 //.withDatabase(2)
