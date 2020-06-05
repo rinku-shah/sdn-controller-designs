09b0bf545d27cc006133614ede8534c9c0da98af
rinku-shah <rinku@cse.iitb.ac.in>
2020-06-05 12:20:23 +0530

    added dgw p4 code

---
+++ hardware-onos/p4-code/dgw/out/pif_design.c
@@ -0,0 +1,6 @@
+/* Copyright (C) 2015-2016,  Netronome Systems, Inc.  All rights reserved. */
+
+#include <stdint.h>
+#include <nfp.h>
+
+__export __mem __addr40 uint32_t pif_design_uuid[4] = {0x67cb6e92, 0xabce001e, 0x36ee11ea, 0x90977f19, };
---
+++ hardware-onos/p4-code/dgw/out/pif_registers.c
@@ -0,0 +1,7 @@
+/* Copyright (C) 2015-2016,  Netronome Systems, Inc.  All rights reserved. */
+
+#include "pif_common.h"
+
+__export __emem __align256 struct pif_header_s3_uplink_egress_port pif_register_s3_uplink_egress_port[65000];
+
+__export __emem __align256 struct pif_header_tun_s1_uplink_egress_port pif_register_tun_s1_uplink_egress_port[65000];
